745224f1d3bdebbf3d8669cf370aeff6de1e038a
yuexiaoxing <1071125135@qq.com>
2017-05-16 14:56:49 +0800

    1

--- helloworld-houtai/Application/Common/Conf/config.php
+++ helloworld-houtai/Application/Common/Conf/config.php
@@ -11,7 +11,11 @@ return array(
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  '',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
-//  'LAYOUT_ON'             =>  ''
+    /*'DB_PREFIX'             =>  '',          //数据库表前缀
+    'DB_FIELDTYPE_CHECK'    =>  false,           //是否进行字段类型检查
+    'DB_FIELDS_CACHE'       =>  true,        //数据库编码默认采用utf8*/
+    
+//  'LAYOUT_ON'             =>  
 	// 'LOG_RECORD'            =>  true,  // 进行日志记录
 	// 'LOG_EXCEPTION_RECORD'  =>  true,    // 是否记录异常信息日志
 	// 'LOG_LEVEL'             =>  'EMERG,ALERT,CRIT,ERR,WARN,NOTIC,INFO,DEBUG,SQL',  // 允许记录的日志级别
